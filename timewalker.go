aa7b4e7f225fc2370c10df7a289f7e6d2f6d7a68
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-10-12 20:13:37 -0400

    WIP

--- timewalker.go
+++ timewalker.go
@@ -1,6 +1,7 @@
 package timewalker
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -9,18 +10,49 @@ type Interval struct {
 	End   time.Time
 }
 
-func TimeWalker() (<-chan Interval, error) {
-	intervals := make(chan Interval)
+func TryAdd() {
+	now := time.Now()
+	fmt.Printf("now:  %v\n", now)
+	soon := now.Add(time.Second)
+	fmt.Printf("now:  %v\n", now)
+	fmt.Printf("soon: %v\n", soon)
 
+}
+
+func (i Interval) String() string {
+	// layout := time.RFC3339
+	layout := "2006-01-02T15:04:05.000Z07:00"
+	return fmt.Sprintf("[%s, %s)", i.Start.Format(layout), i.End.Format(layout))
+}
+
+// Truncate Start, Round up End
+func (i Interval) Round(d time.Duration) Interval {
+	fmt.Printf("-%v\n", i)
+	start := i.Start.Truncate(d)
+	end := i.End.Truncate(d)
+	fmt.Printf("+%v\n", i)
+	fmt.Printf("=%v\n", Interval{Start: start, End: end})
+	return Interval{Start: start, End: end}
+}
+
+func (i Interval) Iter(d time.Duration) (<-chan Interval, error) {
+	ch := make(chan Interval)
+
+	// truncate start to duration
+	ri := i.Round(d)
+
+	fmt.Printf("\n")
+	fmt.Printf("%s\n", time.RFC3339)
+	fmt.Printf(" i: %v\n", i)
+	fmt.Printf("ri: %v\n", ri)
 	go func() {
-		start := time.Now()
-		for i := 0; i < 10; i++ {
-			end := start.Add(time.Second)
-			intervals <- Interval{Start: start, End: end}
+		start := ri.Start
+		for start.Before(ri.End) {
+			end := start.Add(d)
+			ch <- Interval{Start: start, End: end}
 			start = end
 		}
-		close(intervals)
+		close(ch)
 	}()
-
-	return intervals, nil
+	return ch, nil
 }
--- timewalker_test.go
+++ timewalker_test.go
@@ -2,19 +2,29 @@ package timewalker
 
 import (
 	"testing"
+	"time"
 )
 
+func TestAdder(t *testing.T) {
+	TryAdd()
+}
 func TestTimeWalker(t *testing.T) {
-	tw, err := TimeWalker()
+	t.Skip()
+	return
+	// i := Interval{Start: time.Now(), End: time.Now().Add(10 * time.Second)}
+	i := Interval{Start: time.Unix(0, 0), End: time.Unix(10, 5e8)}
+
+	ch, err := i.Iter(time.Second)
 	if err != nil {
 		t.Errorf("TimeWalker generated unexpected error")
 	}
+
 	result := make([]Interval, 0)
-	for interval := range tw {
+	for interval := range ch {
 		result = append(result, interval)
 	}
 
 	if len(result) != 10 {
-		t.Errorf("Generated interval has wrong length")
+		t.Errorf("Generated interval has wrong length: %d", len(result))
 	}
 }
