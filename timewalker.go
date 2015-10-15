16d0c707d1b164b5d98e259ff0c5565024cc638b
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-10-15 12:34:30 -0400

    HDuration

--- timewalker.go
+++ timewalker.go
@@ -5,18 +5,43 @@ import (
 	"time"
 )
 
+// Human Readable Durations (non-arithmetic)
+type HDuration int
+
+const (
+	Day HDuration = iota
+	Month
+	Year
+)
+
+func Round(t time.Time, hd HDuration) time.Time {
+	year, month, day := t.Date()
+	switch hd {
+	case Day:
+		return time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+	case Month:
+		return time.Date(year, month, 1, 0, 0, 0, 0, t.Location())
+	case Year:
+		return time.Date(year, time.January, 1, 0, 0, 0, 0, t.Location())
+	}
+	return t
+
+}
+func (hd HDuration) String() string {
+	switch hd {
+	case Day:
+		return "Day"
+	case Month:
+		return "Month"
+	case Year:
+		return "Year"
+	}
+	return ""
+}
+
 type Interval struct {
 	Start time.Time
 	End   time.Time
-}
-
-func TryAdd() {
-	now := time.Now()
-	fmt.Printf("now:  %v\n", now)
-	soon := now.Add(time.Second)
-	fmt.Printf("now:  %v\n", now)
-	fmt.Printf("soon: %v\n", soon)
-
 }
 
 func (i Interval) String() string {
@@ -25,14 +50,25 @@ func (i Interval) String() string {
 	return fmt.Sprintf("[%s, %s)", i.Start.Format(layout), i.End.Format(layout))
 }
 
+// This normalizes an Interval's representations
+// -swap Start,End if appropriate
 // Truncate Start, Round up End
+// Make sure we have at least one interval.
 func (i Interval) Round(d time.Duration) Interval {
-	fmt.Printf("-%v\n", i)
-	start := i.Start.Truncate(d)
-	end := i.End.Truncate(d)
-	fmt.Printf("+%v\n", i)
-	fmt.Printf("=%v\n", Interval{Start: start, End: end})
-	return Interval{Start: start, End: end}
+	if i.End.Before(i.Start) {
+		i.End, i.Start = i.Start, i.End
+	}
+	i.Start = i.Start.Truncate(d)
+	// truncate End, if truncEnd<End, or truncEnd<Start, add 1 duration
+	truncEnd := i.End.Truncate(d)
+	if truncEnd.Before(i.End) {
+		i.End = truncEnd.Add(d)
+	} else if truncEnd.Before(i.Start.Add(d)) {
+		i.End = i.Start.Add(d)
+	} else {
+		i.End = truncEnd
+	}
+	return i
 }
 
 func (i Interval) Iter(d time.Duration) (<-chan Interval, error) {
--- timewalker_test.go
+++ timewalker_test.go
@@ -5,8 +5,90 @@ import (
 	"time"
 )
 
-func TestAdder(t *testing.T) {
-	TryAdd()
+var durationTests = []struct {
+	hdur HDuration
+	exp  string // expected result
+}{
+	{Day, "Day"},
+	{Month, "Month"},
+	{Year, "Year"},
+}
+
+func TestHDuration(t *testing.T) {
+	for _, tt := range durationTests {
+		actual := tt.hdur.String()
+		if actual != tt.exp {
+			t.Errorf("(%s): exp: %v act: %v", tt.hdur, tt.exp, actual)
+		}
+	}
+}
+func p(a, b string) Interval {
+	lyt := time.RFC3339
+	ta, err := time.Parse(lyt, a)
+	if err != nil {
+		panic(err)
+	}
+	tb, err := time.Parse(lyt, b)
+	if err != nil {
+		panic(err)
+	}
+	return Interval{Start: ta, End: tb}
+}
+
+var roundTests = []struct {
+	inp Interval      // input
+	dur time.Duration // Rounding duration
+	exp Interval      // expected result
+}{
+	{ // already ok
+		inp: p("2000-01-01T00:00:00Z", "2001-01-01T00:00:00Z"),
+		dur: time.Second * 10,
+		exp: p("2000-01-01T00:00:00Z", "2001-01-01T00:00:00Z"),
+	}, { //swap start, end
+		inp: p("2001-01-01T00:00:00Z", "2000-01-01T00:00:00Z"),
+		dur: time.Second * 10,
+		exp: p("2000-01-01T00:00:00Z", "2001-01-01T00:00:00Z"),
+	}, { //round start
+		inp: p("2000-01-01T00:00:06Z", "2001-01-01T00:00:00Z"),
+		dur: time.Second * 10,
+		exp: p("2000-01-01T00:00:00Z", "2001-01-01T00:00:00Z"),
+	}, { // round end - up
+		inp: p("2000-01-01T00:00:00Z", "2001-01-01T00:00:06Z"),
+		dur: time.Second * 10,
+		exp: p("2000-01-01T00:00:00Z", "2001-01-01T00:00:10Z"),
+	}, { // round end - up because before start+d
+		inp: p("2000-01-01T00:00:00Z", "2000-01-01T00:00:06Z"),
+		dur: time.Second * 10,
+		exp: p("2000-01-01T00:00:00Z", "2000-01-01T00:00:10Z"),
+	}, { // Try with Hour - round start, end ok
+		inp: p("2000-01-01T01:23:45Z", "2001-01-01T00:00:00Z"),
+		dur: time.Hour,
+		exp: p("2000-01-01T01:00:00Z", "2001-01-01T00:00:00Z"),
+	}, { // Hour - round end up
+		inp: p("2000-01-01T01:23:45Z", "2001-01-01T01:23:45Z"),
+		dur: time.Hour,
+		exp: p("2000-01-01T01:00:00Z", "2001-01-01T02:00:00Z"),
+	},
+}
+
+func TestRound(t *testing.T) {
+	t.Skip()
+
+	for _, tt := range roundTests {
+		actual := tt.inp.Round(tt.dur)
+		t.Logf("-inp %v\n", tt.inp)
+		t.Logf("-dur %v\n", tt.dur)
+		t.Logf("-act %v\n", actual)
+		t.Logf("-exp %v\n", tt.exp)
+		if actual != tt.exp {
+			t.Errorf("Round(%s):\ninp :%v \nexp: %v, \nact: %v", tt.dur, tt.inp, tt.exp, actual)
+		}
+	}
+	// i := Interval{Start: time.Unix(1e8, 0), End: time.Unix(10, 5e8)}
+	// t.Logf("-i %v\n", i)
+	// r := i.Round(time.Minute)
+	// t.Logf("+i %v\n", i)
+	// t.Logf("+r %v\n", r)
 }
 func TestTimeWalker(t *testing.T) {
 	t.Skip()
